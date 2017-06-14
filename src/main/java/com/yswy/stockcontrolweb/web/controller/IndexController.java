e8365086772fec2fa4e2314b5509579e909f1bef
yswy <458774478@qq.com>
2017-06-14 17:30:52 +0800

    初步整合mybatis

--- src/main/java/com/yswy/stockcontrolweb/web/controller/IndexController.java
+++ src/main/java/com/yswy/stockcontrolweb/web/controller/IndexController.java
@@ -4,11 +4,18 @@
  */
 package com.yswy.stockcontrolweb.web.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.context.request.WebRequest;
+
+import com.yswy.stockcontrolweb.dal.dao.UserMapper;
+import com.yswy.stockcontrolweb.dal.dateobject.User;
+import com.yswy.stockcontrolweb.dal.dateobject.UserExample;
 
 /**
  * 
@@ -20,11 +27,21 @@ import org.springframework.web.context.request.WebRequest;
 @Controller
 @RequestMapping("/index.htm")
 public class IndexController {
+	
+	@Autowired
+	private UserMapper userMapper;
 
 	@RequestMapping(method = RequestMethod.GET)
 	public String doGet(ModelMap model, WebRequest request) {
 		model.addAttribute("test", "这是后台传入的值");
+		testDB(model);
 		return "index.vm";
 	}
 
+	public void testDB(ModelMap mdelMap) {
+		UserExample ss = new UserExample();
+		ss.createCriteria().andUserIdEqualTo("1000");
+		List<User> list = userMapper.selectByExample(ss);
+		mdelMap.addAttribute("list", list);
+	}
 }
